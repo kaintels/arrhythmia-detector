4c6efa36a87c7a2ce0ad2dee2ecc2d9a0c114afe
Seungwoo Han <hanuengine@duduit.co.kr>
2021-05-14 14:24:38 +0900

    Add mit-bih.bat

---
+++ dataset/mit-bih.bat
@@ -0,0 +1,5 @@
+@echo off
+REM echo "Data Download... wait 1 seconds..."
+REM timeout 1
+REM curl.exe --output ".\data\mit-bih-arrhythmia-database-1.0.0.zip" https://storage.googleapis.com/mitdb-1.0.0.physionet.org/mit-bih-arrhythmia-database-1.0.0.zip
+REM echo "Download successfully. wait 3 seconds..."
