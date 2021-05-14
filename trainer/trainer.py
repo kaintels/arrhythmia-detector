8f2112785094b24cdac7686a18b95f90aad0e9f8
Seungwoo Han <hanuengine@duduit.co.kr>
2021-05-14 14:25:54 +0900

    Add trainer.py

---
+++ trainer/trainer.py
@@ -0,0 +1,60 @@
+import math
+import random
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from model.model import ATMNet
+import pickle
+
+random_seed = 777
+
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed)  # if use multi-GPU
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ATMNet().to(device)
+
+with open('../dataset/training/feature.pkl', 'rb') as f:
+    feature = pickle.load(f)
+
+with open('../dataset/training/target.pkl', 'rb') as f:
+    target = pickle.load(f)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+def train(target=feature, label=target, batch_size_num=32, epochs=5):
+    train_dataset = TensorDataset(torch.FloatTensor(target), torch.LongTensor(label).squeeze())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size_num, shuffle=True, drop_last=True)
+    file = open("../log/log.txt", 'w')
+    model.train()
+    check_loss = math.inf
+    for epoch in range(epochs):
+        outputs = []
+        if epoch == 0:
+            print("-" * 100)
+        print("Epoch : {0}".format(epoch+1))
+        print("-" * 100)
+        for idx, (data, target) in enumerate(train_loader):
+            data = data.to(device)
+            target = target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.cross_entropy(output, target)
+            loss.backward()    # calc gradients
+            optimizer.step()   # update gradients
+
+            if loss.item() < check_loss:
+                torch.save(model.state_dict(), "../checkpoint/predictor.pth")
+                check_loss = loss.item() # save optimized loss model
+                print("Save Model. Iteration : {0}, Loss : {1}".format(idx, loss.item()))
+                log = "Save Model. "+"Epoch : "+str(epoch+1)+", Iteration : "+str(idx)+", Loss : "+str(loss.item())+"\n"
+                file.write(log)
+        print("-" * 100)
+
+train(target=feature, label=target, batch_size_num=32, epochs=5)
